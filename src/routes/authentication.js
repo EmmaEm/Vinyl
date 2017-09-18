86b739b4296fe201fa8f92c0ba59dbb82d24f6e8
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-18 12:19:54 -0700

    Add sign-in.ejs to match wireframe & route rendering it

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
-const db = require('../db')
+// const db = require('../db')
 
 router.get('/sign-up', (req, res) => {
   res.render('authentication/sign-up')
+})
+
+router.get('/sign-in', (req, res) => {
+  res.render('authentication/sign-in')
 })
 
 module.exports = router
