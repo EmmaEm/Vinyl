4b1bbaed98d3bbb5f18cf79b3249b0424c8374d2
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-18 12:53:09 -0700

    Add profile page ejs matching wireframes & route rendering it

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const albums = require('./albums.js')
 const authentication = require('./authentication.js')
+const users = require('./users.js')
 
 router.use('/', albums)
 router.use('/', authentication)
+router.use('/', users)
 
 module.exports = router
