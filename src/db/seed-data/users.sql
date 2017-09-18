5d441fe1372dd7ade59b5fa8407b39e644b76498
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-18 13:59:15 -0700

    Add users schema & seed-data

---
+++ src/db/seed-data/users.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+  users (name, email, password)
+VALUES
+  ('Arya Stark', 'NoOne@ravens.com', 'needle'),
+  ('Jon Snow', 'KingOfTheNorth@ravens.com', 'ghost'),
+  ('Daenerys Targaryen', 'Khaleesi@ravens.com', 'dragons')
+  ;
