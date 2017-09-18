2df70d09a7b9434804059f07c2ef067a3a0284c1
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-18 10:13:30 -0700

    Remove pg and add pg promise from dependencies and update all queries to use pg-promise instead of callbacks

--- src/db/index.js
+++ src/db/index.js
@@ -1,33 +1,35 @@
-const pg = require('pg')
+// const pg = require('pg')
+const pgp = require('pg-promise')()
 
 const dbName = 'vinyl'
 const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
-const client = new pg.Client(connectionString)
+// const client = new pg.Client(connectionString)
+const db = pgp(connectionString)
 
-client.connect()
+// client.connect()
 
-function getAlbums(cb) {
-  _query('SELECT * FROM albums', [], cb)
+function getAlbums() {
+  return db.query('SELECT * FROM albums')
 }
 
-function getAlbumsByID(albumID, cb) {
-  _query('SELECT * FROM albums WHERE id = $1', [albumID], cb)
+function getAlbumsByID(albumID) {
+  return db.query('SELECT * FROM albums WHERE id = $1', [albumID])
 }
 
-function _query(sql, variables, cb) {
-  console.log('QUERY ->', sql.replace(/[\n\s]+/g, ' '), variables)
-
-  client.query(sql, variables, (error, result) => {
-    if (error) {
-      console.log('QUERY -> !!ERROR!!')
-      console.error(error)
-      cb(error)
-    } else {
-      console.log('QUERY ->', JSON.stringify(result.rows))
-      cb(error, result.rows)
-    }
-  })
-}
+// function _query(sql, variables, cb) {
+//   console.log('QUERY ->', sql.replace(/[\n\s]+/g, ' '), variables)
+//
+//   client.query(sql, variables, (error, result) => {
+//     if (error) {
+//       console.log('QUERY -> !!ERROR!!')
+//       console.error(error)
+//       cb(error)
+//     } else {
+//       console.log('QUERY ->', JSON.stringify(result.rows))
+//       cb(error, result.rows)
+//     }
+//   })
+// }
 
 module.exports = {
   getAlbums,
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000
 const app = express()
 
 require('ejs')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -15,26 +16,26 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-  db.getAlbums((error, albums) => {
-    if (error) {
-      res.status(500).render('error', {error})
-    } else {
+  db.getAlbums()
+    .then((albums) => {
       res.render('index', {albums})
-    }
-  })
+    })
+    .catch((error) => {
+      res.status(500).render('error', {error})
+    })
 })
 
 app.get('/albums/:albumID', (req, res) => {
   const albumID = req.params.albumID
 
-  db.getAlbumsByID(albumID, (error, albums) => {
-    if (error) {
-      res.status(500).render('error', {error})
-    } else {
+  db.getAlbumsByID(albumID)
+    .then((albums) => {
       const album = albums[0]
       res.render('album', {album})
-    }
-  })
+    })
+    .catch((error) => {
+      res.status(500).render('error', {error})
+    })
 })
 
 app.use((req, res) => {
