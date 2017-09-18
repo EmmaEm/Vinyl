00613a9c3da3e575c33e179fbba3f35cd34e55e6
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-18 13:35:11 -0700

    Create home.js and take home.ejs out of albums folder in views

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const albums = require('./albums.js')
 const authentication = require('./authentication.js')
 const users = require('./users.js')
 const reviews = require('./reviews.js')
+const home = require('./home.js')
 
 router.use('/', albums)
 router.use('/', authentication)
 router.use('/', users)
 router.use('/', reviews)
+router.use('/', home)
 
 module.exports = router
