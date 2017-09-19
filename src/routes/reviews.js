28dc53bc1890a74a56df38295532018527122900
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-19 13:30:35 -0700

    Submitting new review form creates new review and redirects to album page

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const albums = require('../db/queries/albums')
+const reviews = require('../db/queries/reviews')
 
 // NOTE: Everything in this file is only accessible when user is logged in.
 
@@ -8,6 +9,14 @@ router.get('/albums/:albumId/reviews/new', (req, res) => {
     .then((album) => {
       res.render('reviews/new-review', {album})
     })
+})
+
+router.post('/albums/:albumId/reviews/new', (req, res) => {
+  const reviewContent = req.body.content
+  const userId = res.locals.userId
+  const albumId = req.params.albumId
+  reviews.create(reviewContent, userId, albumId)
+    .then(res.redirect(`/albums/${albumId}`))
 })
 
 module.exports = router
