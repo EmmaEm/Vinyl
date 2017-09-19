cb087cbfdb2b50cf98bcb348e85917868f37701c
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-19 11:17:55 -0700

    See last commit message. Some changes didn't save properly before previous commit

--- src/db/queries/albums.js
+++ src/db/queries/albums.js
@@ -1,5 +1,14 @@
 const db = require('../../config/db.js').db
 
-module.exports = {
+function getAll() {
+  return db.query('SELECT * FROM albums')
+}
 
+function getById(albumId) {
+  return db.query('SELECT * FROM albums WHERE id = $1', [albumId])
+}
+
+module.exports = {
+  getAll,
+  getById,
 }
