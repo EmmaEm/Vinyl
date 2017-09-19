59ee7c9d133e315cb0b903e1e23faeae7be9343d
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-18 17:45:41 -0700

    Add logged-in status & user-id of logged-in user to res.locals

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,17 @@ const users = require('./users.js')
 const reviews = require('./reviews.js')
 const home = require('./home.js')
 
+router.use((req, res, next) => {
+  let loggedIn = false
+  let userId = null
+  if (req.session.user) {
+    loggedIn = true
+    userId = req.session.user.user_id
+  }
+  res.locals = {loggedIn, userId}
+  next()
+})
+
 router.use('/', albums)
 router.use('/', users)
 router.use('/', reviews)
